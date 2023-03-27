3f863e3b1194ddccfab7c0cce965f60a394fb54b
AadityakiranS <aadityakirans@gmail.com>
2023-03-27 10:46:08 +0530

    Completed overall task. Need to test for total compliance

--- controllers/api-requests.js
+++ controllers/api-requests.js
@@ -2,13 +2,15 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 s3 = new AWS.S3();
 
+const { processImageToThumbnail } = require('../helpers/thumbnail-processing-helper.js');
 const { createBucketIfNotExists } = require('../helpers/s3-bucket-initialization-helper.js');
+let bucketParams;
 
 const uploadToS3AndProcessThumbnail = async (req, res) => {
     let { fileKey, filePath } = req.body;
     try {
         // Read the file from disk
-        const fileContent = fs.readFileSync(filePath);
+        const imageBuffer = fs.readFileSync(filePath);
 
         // Create the parameters for calling createBucket
         var bucketName = `${process.env.PROJECT_NAME}-${process.env.AWS_REGION}`;
@@ -22,23 +24,45 @@ const uploadToS3AndProcessThumbnail = async (req, res) => {
                 return res.status(500).json({ msg: error.message });
             });
 
-        // Set the parameters for uploading
-        const params = {
+        // Set the parameters for uploading image
+        bucketParams = {
             Bucket: bucketName,
             Key: fileKey,
-            Body: fileContent
+            Body: imageBuffer
         };
 
         // Upload the file to S3
-        s3.upload(params, (err, data) => {
+        s3.upload(bucketParams, (err, data) => {
             if (err) {
                 console.error(err);
             } else {
-                console.log(`File uploaded successfully. File URL: ${data.Location}`);
+                console.log(`Image uploaded successfully. File URL: ${data.Location}`);
             }
         });
 
-        return res.status(200).json({ msg: `File successfully uploaded` });
+        let thumbnailBuffer = await processImageToThumbnail(imageBuffer);
+        if (thumbnailBuffer.success === false) {
+            return res.status(500).json({ msg: `Thumbnail size is too big` });
+        }
+
+        // Set the parameters for uploading image
+        bucketParams = {
+            Bucket: bucketName,
+            Key: `${fileKey.split('.')[0]}_thumbnail.jpeg`,
+            Body: thumbnailBuffer.content,
+            ContentType: 'image/jpeg'
+        };
+
+        // Upload the file to S3
+        s3.upload(bucketParams, (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(`Thumbnail uploaded successfully. File URL: ${data.Location}`);
+            }
+        });
+
+        return res.status(200).json({ msg: `Image and thumbnail sucessfully processed and uploaded` });
     }
     catch (error) {
         return res.status(500).json({ msg: error.message });
---
+++ helpers/thumbnail-processing-helper.js
@@ -0,0 +1,53 @@
+const sharp = require('sharp');
+const fs = require('fs');
+
+let outputObj = { success: false, message: `Yet to be intialized`, content: null };
+
+// Process the image using Sharp
+const processImageToThumbnail = async (buffer) => {
+    try {
+        //#TOASK: Is this the correct way to set the aspect ratio of an image? 
+        //Processing once to change aspect ratio
+        let thumbnailImage = await sharp(buffer)
+            .resize({
+                width: 1000, // set the width to 1000 pixels
+                height: 500, // set the height to half of the width (1:2 aspect ratio)
+                fit: 'fill', // specify how to fit the image in case it doesn't have the exact aspect ratio
+                position: 'center' // specify where to position the image if there's any whitespace
+            })
+            .toFormat("jpeg", { mozjpeg: true })
+            .toBuffer();
+
+        // Reduce the quality setting until the output buffer is below 10KB
+        let outputQuality = 90;
+        while (thumbnailImage.length > 30000 /*#TOASK: How to prevent this hardcoding?*/ && outputQuality >= 10) {
+            thumbnailImage = await sharp(buffer)
+                .toFormat("jpeg", { mozjpeg: true })
+                .jpeg({ quality: outputQuality })
+                .toBuffer();
+            outputQuality -= 10;
+        }
+
+        if (thumbnailImage.length > 30000) {
+            console.log("Image still too big");
+            outputObj.message = `Image is too big to convert to thumbnail within specified size`;
+
+            return outputObj;
+        }
+
+        outputObj.success = true;
+        outputObj.message = `Image successfully converted to thumbnail of appropriate size`;
+        outputObj.content = thumbnailImage;
+
+        return outputObj;
+    } catch (error) {
+        console.log(error);
+
+        outputObj.success = false;
+        outputObj.message = error;
+
+        return outputObj;
+    }
+}
+
+module.exports = { processImageToThumbnail };
